b9912879e5e06d271d7fc4c84c3eddd15933aa91
StanR <hi@stanr.info>
2022-06-14 22:05:38 +0300

    Add stats

--- frontend/src/components/copyright.js
+++ frontend/src/components/copyright.js
@@ -3,12 +3,22 @@ import Typography from '@mui/material/Typography';
 
 export default function Copyright() {
   return (
+      <>
         <Typography
             variant="body2"
             align="center"
             color="text.secondary"
           >
-           Made by <Link href="https://osu.ppy.sh/users/7217455">StanR</Link>, icon by <Link href="https://osu.ppy.sh/users/4411044">Arhella</Link>.
+           Made by <Link href="https://osu.ppy.sh/users/7217455" underline="hover">StanR</Link>, icon by <Link href="https://osu.ppy.sh/users/4411044" underline="hover">Arhella</Link>.
         </Typography>
+        <Typography
+            variant="body2"
+            align="center"
+            color="text.secondary"
+            sx={{mb: 2}}
+          >
+           <Link href="/stats" underline="hover">Stats</Link> | <Link href="https://github.com/stanriders/mutualify" underline="hover">Source code</Link>
+        </Typography>
+      </>
     );
 }
---
+++ frontend/src/pages/stats.js
@@ -0,0 +1,23 @@
+import Typography from '@mui/material/Typography';
+import Head from 'next/head'
+import api from '../lib/api'
+
+export default function Stats({data}) {
+  return (
+    <>
+      <Head>
+        <title>Mutualify - Stats</title>
+      </Head>
+        <Typography variant="body1">
+            Registered users: {data.registeredCount}
+        </Typography>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+    const data = await api(`/stats`);
+    return {
+      props: {data}
+    }
+  }
