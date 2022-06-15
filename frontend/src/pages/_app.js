68097aa28287785b6af17555e98926776a223ffe
StanR <hi@stanr.info>
2022-06-15 12:52:18 +0300

    Enable additional icons

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -27,6 +27,11 @@ export default function MyApp(props) {
       <CacheProvider value={emotionCache}>
         <Head>
           <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <meta name="description" content="Mutualify is a friend list database for osu!" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
