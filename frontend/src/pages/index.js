a4dc2e2ecf5daebe393e22a5a384ad5f8e92bb2b
StanR <hi@stanr.info>
2022-06-15 12:48:18 +0300

    Better main page, more icons

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -10,8 +10,29 @@ export default function Index() {
         <Typography variant="h6" align="center">
             Mutualify is a friend list database for osu!
         </Typography>
-        <Typography variant="body1">
-            You log in, website saves your friend list and then others will be able to see if they're in your friend list. It works the other way too - you can see who added you to their friend list as well!
+        <Typography variant="body1" mb={0.5}>
+            <b>Q</b>: What is this?
+        </Typography>
+        <Typography variant="body1" mb={2}>
+            <b>A</b>: Mutualify is a database for osu! players' friend lists. It stores friend list of every player that logged in as well as some other relevant data.
+        </Typography>
+        <Typography variant="body1" mb={0.5}>
+            <b>Q</b>: How does it work?
+        </Typography>
+        <Typography variant="body1" mb={2}>
+            <b>A</b>: osu! API allows websites to get player's friend list. Mutualify cross-checks friend lists of all registered players to see who follows who.
+        </Typography>
+        <Typography variant="body1" mb={0.5}>
+            <b>Q</b>: Can it steal my account/password/private data? 
+        </Typography>
+        <Typography variant="body1" mb={2}>
+            <b>A</b>: No, it can only access what osu! website shows you when you log in which is your friend list, your public profile data and you friend's public profile data. It will never ask for your login/password or anything like that.
+        </Typography>
+        <Typography variant="body1" mb={0.5}>
+            <b>Q</b>: Why can I only see only some of my followers?
+        </Typography>
+        <Typography variant="body1" mb={1}>
+            <b>A</b>: Mutualify can only find people that logged in before, so spread the word! More registered people means more information about followers.
         </Typography>
     </>
   );
