aa8e0ed828932bc9c8033516c3d3d95e45ce912d
StanR <hi@stanr.info>
2022-08-17 16:58:51 +0300

    Fix token refresh response not deserializing

--- backend/Mutualify/OsuApi/Models/TokenResponse.cs
+++ backend/Mutualify/OsuApi/Models/TokenResponse.cs
@@ -1,18 +1,18 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text.Json.Serialization;
 
 namespace Mutualify.OsuApi.Models;
 
 public class TokenResponse
 {
-    [JsonProperty("access_token")]
+    [JsonPropertyName("access_token")]
     public string AccessToken { get; set; } = null!;
 
-    [JsonProperty("expires_in")]
+    [JsonPropertyName("expires_in")]
     public int ExpiresIn { get; set; }
 
-    [JsonProperty("refresh_token")]
+    [JsonPropertyName("refresh_token")]
     public string RefreshToken { get; set; } = null!;
 
-    [JsonProperty("token_type")]
+    [JsonPropertyName("token_type")]
     public string TokenType { get; set; } = null!;
 }
