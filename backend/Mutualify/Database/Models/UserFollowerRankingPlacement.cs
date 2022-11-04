1659eebbd10af1e5766a9e45855fc74b2b046a82
StanR <hi@stanr.info>
2022-11-04 23:26:45 +0300

    Column name

--- backend/Mutualify/Database/Models/UserFollowerRankingPlacement.cs
+++ backend/Mutualify/Database/Models/UserFollowerRankingPlacement.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mutualify.Database.Models;
 
 [Keyless]
 public class UserFollowerRankingPlacement
 {
+    [Column("row_number")]
     public int RowNumber { get; set; }
 }
