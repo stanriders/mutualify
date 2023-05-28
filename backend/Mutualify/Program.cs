249da35986d80aeb017f7c4d3c4d20eaf2d21133
StanR <hi@stanr.info>
2023-05-28 17:54:57 +0300

    Just use user id

--- backend/Mutualify/Program.cs
+++ backend/Mutualify/Program.cs
@@ -153,7 +153,7 @@ app.UseSerilogRequestLogging(options =>
 {
     options.EnrichDiagnosticContext = (context, httpContext) =>
     {
-        context.Set("User", httpContext.User, true);
+        context.Set("UserId", httpContext.User.Identity?.Name);
     };
 });
 
