c2eb4f771713576b57843d4929898d1529da18f5
StanR <hi@stanr.info>
2024-12-20 02:51:17 +0500

    Update localizations

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     emotion: true,
   },
   i18n: {
-    locales: ["en-US", "fi-FI", "fr-FR", "pl-PL", "ru-RU", "sv-SE", "zh-CN", "zh-TW"],
+    locales: ["be-BY", "cs-CZ", "de-DE", "en-US", "es-ES", "fi-FI", "fr-FR", "ko-KR", "lt-LT", "no-NO", "pl-PL", "pt-BR", "ru-RU", "sv-SE", "tr-TR", "uk-UA", "zh-CN", "zh-TW"],
     defaultLocale: "en-US"
   },
   output: 'standalone'
