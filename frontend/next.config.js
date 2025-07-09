d9653ac4fa805d9128a4a81b7fd137e188207ed9
StanR <hi@stanr.info>
2025-07-10 02:18:25 +0500

    Add ro-RO, sr-SP locales

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     emotion: true,
   },
   i18n: {
-    locales: ["be-BY", "cs-CZ", "de-DE", "en-US", "es-ES", "fi-FI", "fr-FR", "ko-KR", "lt-LT", "no-NO", "pl-PL", "pt-BR", "ru-RU", "sv-SE", "tr-TR", "uk-UA", "zh-CN", "zh-TW"],
+    locales: ["be-BY", "cs-CZ", "de-DE", "en-US", "es-ES", "fi-FI", "fr-FR", "ko-KR", "lt-LT", "no-NO", "pl-PL", "pt-BR", "ro-RO", "ru-RU", "sr-SP", "sv-SE", "tr-TR", "uk-UA", "zh-CN", "zh-TW"],
     defaultLocale: "en-US"
   },
   output: 'standalone'
